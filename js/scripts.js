d3532b7cbddf2c1a0a7f0dd62c7303d47843d5be
IwataTaiki0002 <274156741+shin-12345@users.noreply.github.com>
2026-07-18 11:55:40 +0900

    about-me

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-function showMessage() {
-    document.getElementById("message").textContent =
-        "見てくれてありがとうございます！";
+const colors = [
+    "#eef2f7",
+    "#fff8e7",
+    "#eafaf1",
+    "#f5ecff",
+    "#e8f7ff"
+];
+
+let index = 0;
+
+function changeColor(){
+    index++;
+
+    if(index >= colors.length){
+        index = 0;
+    }
+
+    document.body.style.background = colors[index];
 }
